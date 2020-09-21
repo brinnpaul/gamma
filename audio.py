1a26753bda1cb6d6868c0d70bd6b309d9482b666
Brinn Riordan <brinn.riordan@gmail.com>
2020-09-20 21:14:01 -0400

    improve naming

--- audio.py
+++ audio.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pyaudio
 import select
-import array
 import sys
 
 
 class Hertz(object):
 
-    def __init__(self, volume=0.5, duration=5, rate=44100, frequency=40.0):
-        self._duration=duration   # in seconds, may be float
-        self._rate=rate           # sampling rate, Hz, must be integer
-        self._frequency=frequency # sine frequency, Hz, must be float
+    def __init__(self, channels=2, duration=5, rate=48000, frequency=40.0):
+        self._channels = channels
+        self._duration = duration    # in seconds, may be float
+        self._rate = rate            # sampling rate, Hz, must be integer
+        self._frequency = frequency  # sine frequency, Hz, must be float
         self._p = pyaudio.PyAudio()
         self._stream = None
 
@@ -23,15 +23,15 @@ class Hertz(object):
         self._stream.close()
         self._p.terminate()
 
-    def _stream(self):
+    def _generate_stream(self):
         pass
 
     def sound(self):
         pass
 
     @staticmethod
-    def generate(type):
-        if type is None or type != 'b':
+    def generate(impl):
+        if impl is None or impl != 'b':
             print('Generating NonBlocking Stream')
             return NonBlocking()
         else:
@@ -42,19 +42,19 @@ class Hertz(object):
 class Blocking(Hertz):
 
     def __init__(self, *args, **kwargs):
-        super(Blocking, self).__init__(kwargs) # python black magic
+        super(Blocking, self).__init__(*args, **kwargs)
 
     def _generate_stream(self):
         self._stream = self._p.open(
             format=pyaudio.paFloat32,
-            channels=1,
+            channels=self._channels,
             rate=self._rate,
             output=True)
 
     def _generate_sample(self):
-        raw_sample = self._rate*self._duration
-        scale = self._frequency/self._rate
-        return (np.sin(2*np.pi*np.arange(raw_sample)*scale)).astype(np.float32)
+        sample_range = self._rate*self._duration*self._channels
+        scale = (2/self._channels)*np.pi*(self._frequency/self._rate)
+        return (np.sin(np.arange(sample_range)*scale)).astype(np.float32)
 
     def sound(self):
         sample = self._generate_sample()
@@ -64,15 +64,15 @@ class Blocking(Hertz):
 class NonBlocking(Hertz):
 
     def __init__(self, *args, **kwargs):
-        super(NonBlocking, self).__init__(kwargs) # python black magic
+        super(NonBlocking, self).__init__(*args, **kwargs)
         self._frames_per_buffer = 2048
-        self._outbuf = array.array('f',range(self._frames_per_buffer))
+        self._out_buffer = np.zeros(self._frames_per_buffer * 2).astype(np.float32)
         self._phase = 0
 
     def _generate_stream(self):
         self._stream = self._p.open(
             format=pyaudio.paFloat32,
-            channels=1,
+            channels=self._channels,
             rate=self._rate,
             frames_per_buffer=self._frames_per_buffer,
             input=False,
@@ -80,24 +80,24 @@ class NonBlocking(Hertz):
             stream_callback=self._callback)
 
     def _callback(self, in_data, frame_count, time_info, status):
-        for n in range(frame_count):
-            self._outbuf[n] = np.sin(self._phase)
-            self._phase += 2*np.pi*self._frequency/self._rate
-
-        return (np.array(self._outbuf).astype(np.float32),pyaudio.paContinue)
+        scale = self._frequency/self._rate
+        for n in range(frame_count * self._channels):
+            self._out_buffer[n] = np.sin(self._phase)
+            self._phase += (2/self._channels)*np.pi*scale
+        return self._out_buffer, pyaudio.paContinue
 
     def _update_frequency(self, new_frequency):
         new_frequency = new_frequency.rstrip('\n')
         self._frequency = float(new_frequency)
-        stmt = 'Set new frequency to: {new_f} hz\n'.format(new_f=new_frequency)
+        stmt = 'Set frequency to: {new_f} hz\n'.format(new_f=new_frequency)
         print(stmt, end='')
 
     def sound(self):
         while self._stream.is_active():
             while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-              new_f = sys.stdin.readline()
-              if new_f:
-                self._update_frequency(new_f)
+                new_frequency = sys.stdin.readline()
+                if new_frequency:
+                    self._update_frequency(new_frequency)
 
 
 if __name__ == '__main__':
